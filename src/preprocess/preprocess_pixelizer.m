4df33f8f6f88aedab1aa962e5bffc469a83d1be5
hugohutri <55588133+hugohutri@users.noreply.github.com>
2021-12-01 21:47:33 +0200

    refactor: make anti-aliasing cleaner

--- src/preprocess/preprocess_pixelizer.m
+++ src/preprocess/preprocess_pixelizer.m
@@ -8,6 +8,8 @@ function pixelized_data = preprocess_pixelizer(data, dimensions)
     % Helper function for rounding away from 0
     ceilfix = @(x)ceil(abs(x)).*sign(x);
 
+    is_inside_image = @(x) 0 <= x && x <= dimensions;
+
     for data_index = 1:length(data)
         % Get one sample from data using index.
         entry = data{data_index};
@@ -19,35 +21,41 @@ function pixelized_data = preprocess_pixelizer(data, dimensions)
         for data_point = 1:length(entry)
             entry_point = entry(data_point, :);
             
+            % Original way of calculating pixels
             % % Calculate pixel coordinates.
             % x = round(entry_point(1) * dimensions) + 1;
             % y = round(entry_point(2) * dimensions) + 1;
             % pixel_matrix(x, y) = 1;
 
-            % Calculate pixel coordinates.
+            % Calculate pixel coordinates with anti-aliasing:
+            %
+            % Sorry about this mess
+            %
+            % Position of the pixel as floating point number
             x_float = entry_point(1) * dimensions + 1;
             y_float = entry_point(2) * dimensions + 1;
-
-            % x_mod = mod(x_float, 1);
-            % y_mod = mod(y_float, 1);
-
+            % Actual coordinate of the main pixel
             x = round(x_float);
             y = round(y_float);
-
+            % What is the rounding error when pixelasing? 0-0.5
             x_diff = x_float - x;
             y_diff = y_float - y;
-
+            % x and y directions of the roundind error: -1 or 1
             x_dir = ceilfix(x_diff);
             y_dir = ceilfix(y_diff);
-
+            % Set the main pixel to be 1 (The point was inside this pixel)
             pixel_matrix(x, y) = 1;
-            try
+            % Set adjanced pixels to be gray based on the distance from the point.
+            % The distance can be 0-0.5, we multiply that distance by 2 to be 0-1
+            x_is_inside = is_inside_image(x+x_dir);
+            y_is_inside = is_inside_image(y+y_dir);
+            if (x_is_inside) 
                 pixel_matrix(x+x_dir, y) = max(pixel_matrix(x+x_dir, y), 2*(abs(x_diff)));
             end
-            try
+            if (y_is_inside)
                 pixel_matrix(x, y+y_dir) = max(pixel_matrix(x, y+y_dir), 2*(abs(y_diff)));
             end
-            try
+            if (x_is_inside && y_is_inside)
                 pixel_matrix(x+x_dir, y+y_dir) = max(pixel_matrix(x+x_dir, y+y_dir), 2*hypot(x_diff, y_diff));
             end
 
